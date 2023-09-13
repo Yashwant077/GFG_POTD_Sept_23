85d78e24eb3c8f11cd8d19540955961115519eb9
Yashwant Choudhary <84570757+Yashwant077@users.noreply.github.com>
2023-09-13 19:21:37 +0530

    Create 13-09-23-Largest_number_possible.cpp

---
+++ 13-09-23-Largest_number_possible.cpp
@@ -0,0 +1,85 @@
+/*
+Problem Description: Largest number possible
+
+Given two numbers 'N' and 'S' , find the largest number that can be formed with 'N' digits and whose sum of digits should be equals to 'S'. 
+Return -1 if it is not possible.
+
+Example 1:
+
+Input: N = 2, S = 9
+Output: 90
+Explaination: It is the biggest number 
+with sum of digits equals to 9.
+
+Example 2:
+
+Input: N = 3, S = 20
+Output: 992
+Explaination: It is the biggest number 
+with sum of digits equals to 20.
+Your Task:
+You do not need to read input or print anything. Your task is to complete the function findLargest() which takes N and S as input parameters and returns the largest possible number. Return -1 if no such number is possible.
+
+Expected Time Complexity: O(N)
+Exepcted Auxiliary Space: O(1)
+
+Constraints:
+1 ≤ N ≤ 10^4
+0 ≤ S ≤ 10^5
+
+*/
+
+
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    string findLargest(int N, int S){
+        // code here
+        
+        if(S==0 && N>1)
+            return "-1";
+        if(S==0 && N==1)
+            return "0";
+        
+        string ans="";
+        int i=9;
+        
+        while(ans.size()!=N){
+            if(S>=i){
+                S-=i;
+                ans+=to_string(i);
+            }
+            else
+                i--;
+        }
+        
+        if(S==0)
+            return ans;
+        
+        return "-1";
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N, S;
+        cin>>N>>S;
+        
+        Solution ob;
+        cout<<ob.findLargest(N, S)<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
