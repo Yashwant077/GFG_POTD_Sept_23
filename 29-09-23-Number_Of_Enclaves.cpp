1ed03cb44d92c873e5b52857628e560428026d9a
Yashwant Choudhary <84570757+Yashwant077@users.noreply.github.com>
2023-09-29 14:10:30 +0530

    Create 29-09-23-Number_Of_Enclaves.cpp

---
+++ 29-09-23-Number_Of_Enclaves.cpp
@@ -0,0 +1,123 @@
+/*
+Problem Description: Number Of Enclaves
+
+You are given an n x m binary matrix grid, where 0 represents a sea cell and 1 represents a land cell.
+
+A move consists of walking from one land cell to another adjacent (4-directionally) land cell or walking off the boundary of the grid.
+
+Find the number of land cells in grid for which we cannot walk off the boundary of the grid in any number of moves.
+
+Example 1:
+
+Input:
+grid[][] = {{0, 0, 0, 0},
+            {1, 0, 1, 0},
+            {0, 1, 1, 0},
+            {0, 0, 0, 0}}
+Output:
+3
+Explanation:
+0 0 0 0
+1 0 1 0
+0 1 1 0
+0 0 0 0
+The highlighted cells represents the land cells.
+
+Example 2:
+
+Input:
+grid[][] = {{0, 0, 0, 1},
+            {0, 1, 1, 0},
+            {0, 1, 1, 0},
+            {0, 0, 0, 1},
+            {0, 1, 1, 0}}
+Output:
+4
+Explanation:
+0 0 0 1
+0 1 1 0
+0 1 1 0
+0 0 0 1
+0 1 1 0
+The highlighted cells represents the land cells.
+Your Task:
+
+You don't need to print or input anything. Complete the function numberOfEnclaves() which takes a 2D integer matrix grid as the input parameter and returns an integer, denoting the number of land cells.
+
+Expected Time Complexity: O(n * m)
+
+Expected Space Complexity: O(n * m)
+
+Constraints:
+
+1 <= n, m <= 500
+grid[i][j] == 0 or 1
+
+*/
+
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+   public:
+   int n,m;
+   void dfs(int i,int j,vector<vector<int>> &grid){
+      grid[i][j]=2;
+      for(int k=0;k<4;k++){
+          int ni=i+dir[k][0];
+          int nj=j+dir[k][1];
+          if(ni>=0 and ni<n and nj>=0 and nj<m and grid[ni][nj]==1){
+              dfs(ni,nj,grid);
+          }
+      }
+   }
+   vector<vector<int>>dir={{-1,0},{0,1},{1,0},{0,-1}};
+    int numberOfEnclaves(vector<vector<int>> &grid) {
+        // Code here
+         n=grid.size(),m=grid[0].size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if((i==0 or i==n-1 or j==0 or j==m-1) and grid[i][j]==1){
+                    dfs(i,j,grid);
+                }
+            }
+        }
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1){
+                    cnt++;
+                }
+            }
+        }
+        return cnt;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        vector<vector<int>> grid(n, vector<int>(m));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                cin >> grid[i][j];
+            }
+        }
+        Solution obj;
+        cout << obj.numberOfEnclaves(grid) << endl;
+    }
+}
+// } Driver Code Ends
